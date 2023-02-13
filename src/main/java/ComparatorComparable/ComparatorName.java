0841f51de9685cb7f08301805909012fc1f324df
Riya Modi <riyakothwala@gmail.com>
2023-02-13 16:06:52 -0500

    Added ComparatorName class

---
+++ src/main/java/ComparatorComparable/ComparatorName.java
@@ -0,0 +1,11 @@
+package ComparatorComparable;
+
+import java.util.Comparator;
+
+public class ComparatorName implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
